c5de2ece61155e94043d78dfee7ca2a3072844fd
EgorTabakov <64773505+EgorTabakov@users.noreply.github.com>
2021-05-05 17:32:28 +0300

    Add files via upload

---
+++ DateMapp/Storage/StorageAdapter.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DataMapper\Storage;
+
+use DataMapper\Contract\StorageAdapterInterface;
+
+class StorageAdapter implements StorageAdapterInterface
+{
+    public function find(int $id): ?array
+    {
+        foreach ($this->getDataFromSource(['id' => $id]) as $user) {
+            if ($user['id'] === $id) {
+                return $user;
+            }
+        }
+
+        return null;
+    }
+
+    public function findByLogin(string $login): ?array
+    {
+        foreach ($this->getDataFromSource(['login' => $login]) as $user) {
+            if ($user['login'] === $login) {
+                return $user;
+            }
+        }
+
+        return null;
+    }
+
+    public function getDataFromSource($search): array
+    {
+        $admin = ['id' => 1, 'title' => 'Super Admin', 'role' => 'admin'];
+        $user = ['id' => 1, 'title' => 'Main user', 'role' => 'user'];
+        $test = ['id' => 1, 'title' => 'For test needed', 'role' => 'test'];
+
+        $dataSource = [
+            [
+                'id' => 1,
+                'name' => 'Super Admin',
+                'login' => 'root',
+                'password' => '$2y$10$GnZbayyccTIDIT5nceez7u7z1u6K.znlEf9Jb19CLGK0NGbaorw8W', // 1234
+                'role' => $admin
+            ],
+            [
+                'id' => 2,
+                'name' => 'Doe John',
+                'login' => 'doejohn',
+                'password' => '$2y$10$j4DX.lEvkVLVt6PoAXr6VuomG3YfnssrW0GA8808Dy5ydwND/n8DW', // qwerty
+                'role' => $user
+            ],
+            [
+                'id' => 3,
+                'name' => 'Ivanov Ivan Ivanovich',
+                'login' => 'i**extends',
+                'password' => '$2y$10$TcQdU.qWG0s7XGeIqnhquOH/v3r2KKbes8bLIL6NFWpqfFn.cwWha', // PaSsWoRd
+                'role' => $user
+            ],
+            [
+                'id' => 4,
+                'name' => 'Test Testov Testovich',
+                'login' => 'testok',
+                'password' => '$2y$10$vQvuFc6vQQyon0IawbmUN.3cPBXmuaZYsVww5csFRLvLCLPTiYwMa', // testss
+                'role' => $test
+            ],
+        ];
+
+        if (!count($search)) {
+            return $dataSource;
+        }
+
+        $productFilter = function (array $dataSource) use ($search): bool {
+            return (bool)array_intersect($dataSource, $search);
+        };
+
+        return array_filter($dataSource, $productFilter);
+    }
+}
